da7883abf8c333bcc8ad80b4a299a8481013fbf4
Hajeong Jeon <62226517+hajeong815@users.noreply.github.com>
2024-11-22 10:50:26 +0100

    Create matrix_multiply.h

---
+++ matrix_multiply.h
@@ -0,0 +1,8 @@
+#ifndef MATRIX_MULTIPLY_H
+#define MATRIX_MULTIPLY_H
+
+#include "matrix/matrix.h"
+
+void matrix_multiply(const Matrix *a, const Matrix *b, Matrix *result);
+
+#endif // MATRIX_MULTIPLY_H
