a32e1bf3640add16ea9e87515c63cc7efd8649dc
Hajeong Jeon <62226517+hajeong815@users.noreply.github.com>
2024-11-22 10:51:00 +0100

    Create matrix_subtract.h

---
+++ matrix_subtract.h
@@ -0,0 +1,8 @@
+#ifndef MATRIX_SUBTRACT_H
+#define MATRIX_SUBTRACT_H
+
+#include "matrix/matrix.h"
+
+void matrix_subtract(const Matrix *a, const Matrix *b, Matrix *result);
+
+#endif // MATRIX_SUBTRACT_H
