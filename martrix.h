173311cad121580758158b5270fa46b451f93d6f
Hajeong Jeon <62226517+hajeong815@users.noreply.github.com>
2024-11-22 10:18:48 +0100

    Update martrix.h

--- martrix.h
+++ martrix.h
@@ -1,9 +1,15 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-#define MATRIX_SIZE 10  // Example size for simplicity
+#define MATRIX_SIZE 3  // Example size
 
-void matrix_add(int a[MATRIX_SIZE][MATRIX_SIZE], int b[MATRIX_SIZE][MATRIX_SIZE], int result[MATRIX_SIZE][MATRIX_SIZE]);
-void matrix_multiply(int a[MATRIX_SIZE][MATRIX_SIZE], int b[MATRIX_SIZE][MATRIX_SIZE], int result[MATRIX_SIZE][MATRIX_SIZE]);
+typedef struct {
+    int rows;
+    int cols;
+    double data[MATRIX_SIZE][MATRIX_SIZE];
+} Matrix;
 
-#endif
+void matrix_add(const Matrix *a, const Matrix *b, Matrix *result);
+void matrix_multiply(const Matrix *a, const Matrix *b, Matrix *result);
+
+#endif // MATRIX_H
