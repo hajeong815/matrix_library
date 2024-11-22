a1045d845a93886bdbe660aa97b914a3f29198da
Hajeong Jeon <62226517+hajeong815@users.noreply.github.com>
2024-11-22 10:13:20 +0100

    Create martrix.h

---
+++ martrix.h
@@ -0,0 +1,9 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#define MATRIX_SIZE 10  // Example size for simplicity
+
+void matrix_add(int a[MATRIX_SIZE][MATRIX_SIZE], int b[MATRIX_SIZE][MATRIX_SIZE], int result[MATRIX_SIZE][MATRIX_SIZE]);
+void matrix_multiply(int a[MATRIX_SIZE][MATRIX_SIZE], int b[MATRIX_SIZE][MATRIX_SIZE], int result[MATRIX_SIZE][MATRIX_SIZE]);
+
+#endif
